af0df9b52ca3ec4846c60998d46302c14234a262
María Mercedes Retolaza <mretolaza@stsa.info>
2018-06-01 08:19:14 -0600

    Ultimas tablas

---
+++ Simulador/Track_table.py
@@ -0,0 +1,9 @@
+# /**************************************************************
+# ** Universidad del Valle de Guatemala                        **
+# ** Bases de Datos                                            **
+# ** María MercedesRetolaza Reyna                              **
+# ** Carne: 16339                                              **
+# ** Fecha: 1/06/2018                                         **
+# ***************************************************************/
+
+import psycopg2
