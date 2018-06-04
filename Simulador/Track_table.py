451fbf5b4aedca33fa2203949d9e6d4043d2e56a
María Mercedes Retolaza Reyna <ret16339@uvg.edu.gt>
2018-06-03 18:03:04 -0600

    delete files

--- Simulador/Track_table.py
+++
@@ -1,9 +0,0 @@
-# /**************************************************************
-# ** Universidad del Valle de Guatemala                        **
-# ** Bases de Datos                                            **
-# ** María MercedesRetolaza Reyna                              **
-# ** Carne: 16339                                              **
-# ** Fecha: 1/06/2018                                         **
-# ***************************************************************/
-
-import psycopg2
